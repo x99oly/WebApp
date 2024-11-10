01dbea435d771dc9c4e3b117331aa153c864415d
Sameu Araujo <oliveira.samuel.edu@gmail.com>
2024-11-10 12:12:40 -0300

    Serviço de Login criado

---
+++ Domain/DomainSrv/LoginService.cs
@@ -0,0 +1,27 @@
+﻿using WebApp.Aid;
+using WebApp.Domain.Entities;
+using WebApp.Persistence.MySql;
+
+namespace WebApp.Domain.DomainSrv
+{
+    public class LoginService
+    {
+        private MySqlPersistence _mySql;
+        public LoginService() {
+            _mySql = new MySqlPersistence();
+        }
+
+        internal async Task<User> Logar(string email, string password)
+        {
+            var user = await _mySql.GetByEmailAsync<User>(email);
+
+            if (user == null) throw new NullReferenceException(nameof(user));
+
+            string hashPassword = Hashs.HashPassword(password + user.cod);
+
+            if (hashPassword == user.password) return user;
+
+            return null;
+        }
+    }
+}
--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Domain.DomainSrv;
 using WebApp.Domain.Entities;
 using WebApp.Persistence.MySql;
 
@@ -8,13 +9,13 @@ namespace WebApp.Pages
     [BindProperties]
     public class LoginModel : PageModel
     {
-        private MySqlPersistence _sql = new MySqlPersistence();
+        private LoginService _loginService = new LoginService();
         internal User user { get; set; }
         public string email { get; set; }
         public string password { get; set; }
         public async Task OnPost()
         {
-           user = await _sql.GetByEmailAsync<User>(email);
+           user = await _loginService.Logar(email, password);
         }
     }
 }
